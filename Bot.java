a84b1860a321a284889900f31dfbd5e8a765de5c
Daniel Stevens <35317235+dansteves@users.noreply.github.com>
2020-02-01 11:27:39 -0800

    Add files via upload
    Changed the bot in a way so that anyone can make their bot their own chosen name.

--- Bot.java
+++ Bot.java
@@ -1,14 +1,89 @@
 import org.jibble.pircbot.*;
+import java.util.*;
+import java.io.*;
+//import PircBot.*;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 
 public class Bot extends PircBot
 {
-   public Bot()
+   static File oldoutput, output, botUserName;
+   static String botName = "";
+   static ArrayList<String> users = new ArrayList<String>();
+   public Bot() throws FileNotFoundException, IOException
    {
-      this.setName("DStevesBot");
+      //Make sure you have a file named MyBot.txt that only has the UserName you set for the bot.
+      handleUser();
+      loadUserName();
+      this.setName(botName);
       this.isConnected();
       System.out.println("connected!!!!");
+   }
+   public static void printDistinct(ArrayList<String> users) 
+   { 
+        String [] completedUsers = users.toArray(new String[users.size()]);
+        for (int i = 0; i < completedUsers.length; i++) 
+        { 
+            int j; 
+            for (j = 0; j < i; j++) 
+            if (completedUsers[i].equals(completedUsers[j])) 
+                break; 
+            if (i == j) 
+            System.out.println(completedUsers[i]); 
+        } 
+   }
+   public static boolean handleFile() throws IOException
+   {
+     oldoutput = new File("UserList.txt");
+     oldoutput.createNewFile();
+     return true;
+   }
+   public static boolean handleUser() throws IOException
+   {
+     botUserName = new File("MyBot.txt");
+     botUserName.createNewFile();
+     return true;
+   }
+   public static void saveMenu()
+   {
+         String [] completedUsers = users.toArray(new String[users.size()]);
+         String result = "";
+         for(int i = 0; i < completedUsers.length; i++)
+         {
+            result = result + completedUsers[i].toString();
+            result = result + '\n';
+         }
+         oldoutput = new File("UserList.txt");
+         if(oldoutput.canRead() == true)
+         {
+            oldoutput.delete();
+         }
+         output = new File("UserList.txt");
+         try
+         {
+            FileWriter fW = new FileWriter(output, false);
+            fW.write(result);
+            fW.close();
+         }
+         catch (IOException e) { e.printStackTrace(); }
+   }
+   public static void loadMenu() throws FileNotFoundException
+   {
+         Scanner in = new Scanner(oldoutput);
+         users = new ArrayList<String>();
+         while(in.hasNextLine())
+         {
+            String nL = in.nextLine();
+            users.add(nL);
+         }
+   }
+   public static void loadUserName() throws FileNotFoundException
+   {
+         Scanner in = new Scanner(botUserName);
+         if(in.hasNextLine())
+         {
+            botName = in.nextLine();
+         }
    }
    public void onMessage(String channel, String sender, String login, String hostname, String message)
    {
@@ -17,14 +92,79 @@ public class Bot extends PircBot
       {
          sendMessage(channel, "The current game is " + Config.curGame);
       }
+      if(message.equalsIgnoreCase("!interest"))
+      {
+         try
+         {
+            handleFile();
+            loadMenu();
+            
+            String [] completedUsers = users.toArray(new String[users.size()]);
+            if(completedUsers.length == 0)
+            {
+               users.add(sender);
+               sendMessage(channel, sender + " added to list of players interested in me hosting an easy endless tournament"); 
+            }
+            else
+            {
+               for (int i = 0; i < completedUsers.length; i++) 
+               { 
+                  if(completedUsers[i].equals(sender)) 
+                  {
+                      sendMessage(channel, sender + " is already in the list!");
+                      break; 
+                  }
+                  if(i == completedUsers.length - 1)
+                  {
+                     users.add(sender);
+                     sendMessage(channel, sender + " added to list of players interested in me hosting an easy endless tournament");  
+                  }
+               } 
+            }
+                   
+            saveMenu();
+         }
+         catch (Exception f){};
+      }
+      if(message.equalsIgnoreCase("!list"))
+      {
+         try
+         {
+            handleFile();
+            loadMenu();
+            
+            String [] completedUsers = users.toArray(new String[users.size()]);
+            String list = "Players interested in the easy endless tournament: ";
+            int n = completedUsers.length;
+            if(n == 0)
+            {
+               sendMessage(channel, "The current list of users interested is empty");   
+            }
+            else
+            {
+               for (int i = 0; i < n; i++) 
+               { 
+                  list += completedUsers[i];
+                  if(i < n - 1)
+                  {
+                     System.out.println(n + " " + i);
+                     list += ", "; 
+                  }
+               }
+               sendMessage(channel, list);
+            }
+            saveMenu();
+         }
+         catch (Exception f){};
+      }
       if(message.equalsIgnoreCase("!up"))
       {
          try
          {
             Robot robot = new Robot();
-            robot.keyPress(KeyEvent.VK_9);
+            robot.keyPress(KeyEvent.VK_8);
             robot.delay(50);
-            robot.keyRelease(KeyEvent.VK_9);
+            robot.keyRelease(KeyEvent.VK_8);
             System.out.println("up");
          }
          catch(Exception g){};
@@ -46,9 +186,9 @@ public class Bot extends PircBot
          try
          {
             Robot robot = new Robot();
-            robot.keyPress(KeyEvent.VK_3);
+            robot.keyPress(KeyEvent.VK_4);
             robot.delay(50);
-            robot.keyRelease(KeyEvent.VK_3);
+            robot.keyRelease(KeyEvent.VK_4);
             System.out.println("left");
          }
          catch(Exception g){};
@@ -58,9 +198,9 @@ public class Bot extends PircBot
          try
          {
             Robot robot = new Robot();
-            robot.keyPress(KeyEvent.VK_4);
+            robot.keyPress(KeyEvent.VK_6);
             robot.delay(50);
-            robot.keyRelease(KeyEvent.VK_4);
+            robot.keyRelease(KeyEvent.VK_6);
             System.out.println("right");
          }
          catch(Exception g){};
@@ -70,9 +210,9 @@ public class Bot extends PircBot
          try
          {
             Robot robot = new Robot();
-            robot.keyPress(KeyEvent.VK_5);
+            robot.keyPress(KeyEvent.VK_3);
             robot.delay(50);
-            robot.keyRelease(KeyEvent.VK_5);
+            robot.keyRelease(KeyEvent.VK_3);
             System.out.println("a");
          }
          catch(Exception g){};
@@ -82,9 +222,9 @@ public class Bot extends PircBot
          try
          {
             Robot robot = new Robot();
-            robot.keyPress(KeyEvent.VK_6);
+            robot.keyPress(KeyEvent.VK_0);
             robot.delay(50);
-            robot.keyRelease(KeyEvent.VK_6);
+            robot.keyRelease(KeyEvent.VK_0);
             System.out.println("b");
          }
          catch(Exception g){};
@@ -106,9 +246,9 @@ public class Bot extends PircBot
          try
          {
             Robot robot = new Robot();
-            robot.keyPress(KeyEvent.VK_8);
+            robot.keyPress(KeyEvent.VK_9);
             robot.delay(50);
-            robot.keyRelease(KeyEvent.VK_8);
+            robot.keyRelease(KeyEvent.VK_9);
             System.out.println("start");
          }
          catch(Exception g){};
--- Config.java
+++ Config.java
@@ -1,12 +1,18 @@
 public class Config
 {
-   public static String curGame = "MM2";
+   public static String curGame = "";
    public static void main(String [] args) throws Exception
    {
+      //You must run three aruments: your twitch username, the oauth key, and the name of the game you are playing
+      String user = "#" + args[0];
+      String auth = args[1];
+      curGame = args[2];
       Bot bot = new Bot();
       bot.setVerbose(true);
-      bot.connect("irc.twitch.tv", 6667, "oauth:jfo3kvk5s471hy0n07axsjc240e53y");
-      bot.joinChannel("#dsteves");
+      bot.connect("irc.twitch.tv", 6667, auth);
+      bot.sendRawLine("CAP REQ :twitch.tv/membership");
+      bot.sendRawLine("CAP REQ :twitch.tv/commands");
+      bot.joinChannel(user);
       bot.getName();
    }
 }
