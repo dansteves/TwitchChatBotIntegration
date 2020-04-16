7f666460646fdb97ee2044aed373ae0527846ad8
Daniel Stevens <35317235+dansteves@users.noreply.github.com>
2020-04-16 15:26:59 -0700

    Add files via upload
    Bunch of work done to include new chat commands such as hydration bot functionality and dealing with "!add" spam due to Jaku's warp world.

--- Bot.java
+++ Bot.java
@@ -4,12 +4,22 @@ import java.io.*;
 //import PircBot.*;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class Bot extends PircBot
 {
    static File oldoutput, output, botUserName;
    static String botName = "";
    static ArrayList<String> users = new ArrayList<String>();
+   static int hours = 0;
+   static boolean hydration = false;
+   static ScheduledExecutorService eS = Executors.newScheduledThreadPool(1);
+   static long lastexecution = System.currentTimeMillis();
+   static int timethreshold = 3600;
+   static String lastadder = "";
+   static boolean addcheck = false;
    public Bot() throws FileNotFoundException, IOException
    {
       //Make sure you have a file named MyBot.txt that only has the UserName you set for the bot.
@@ -17,7 +27,7 @@ public class Bot extends PircBot
       loadUserName();
       this.setName(botName);
       this.isConnected();
-      System.out.println("connected!!!!");
+      System.out.println("Bot is now connected to the channel.");
    }
    public static void printDistinct(ArrayList<String> users) 
    { 
@@ -87,10 +97,74 @@ public class Bot extends PircBot
    }
    public void onMessage(String channel, String sender, String login, String hostname, String message)
    {
-      System.out.printf("Scanning message from channel %s, sender %s, login %s, hostname %s, message %s. \n", channel, sender, login, hostname, message);
-      if(message.equalsIgnoreCase("!test"))
+      System.out.printf("Scanning message from channel %s, sender %s, login %s, hostname %s, message: %s    . \n", channel, sender, login, hostname, message);
+      String chanmatch = "#" + sender;
+      String chancheck = "#" + lastadder;
+      if(message.equalsIgnoreCase("!gamename"))
       {
          sendMessage(channel, "The current game is " + Config.curGame);
+      }
+      if(hydration == false && ((message.equalsIgnoreCase("!hydrate") && channel.equals(chanmatch)) || (message.contains("is now live!") && sender.equals("streamelements"))))
+      {
+         //System.out.println("Hydrating.");
+         //sendMessage(channel, "Yes, master. ");
+         //ScheduledExecutorService eS = Executors.newSingleThreadScheduledExecutor();
+         //eS.scheduleAtFixedRate(Bot::runTimer, 0 , 5, TimeUnit.SECONDS);
+           hydration = true;
+           eS.scheduleAtFixedRate(new Runnable() 
+           {
+               
+               public void run() 
+               {
+                  long curTime = System.currentTimeMillis();
+                  long diff = curTime - lastexecution;
+                  //System.out.println(diff);
+                  if(hydration == true && (hours == 0 || (diff >= (timethreshold * 99))))
+                  {
+                     lastexecution = System.currentTimeMillis();
+                     int oz = 4*hours;
+                     int ml = 120*hours;
+                     String hydratemessage = "You've been live for just over " + hours;
+                     hydratemessage += " hours. By this point in your broadcast you should have consumed at least ";
+                     hydratemessage += oz;
+                     hydratemessage += "oz (";
+                     hydratemessage += ml;
+                     hydratemessage += "mL) of water to maintain optimum hydration. ";
+                     //hydratemessage += new java.util.Date();
+                     sendMessage(channel, hydratemessage);
+                     hours++;
+                  }
+               }
+           }, 0, timethreshold, TimeUnit.SECONDS);
+      }
+      if(hydration == true && (message.equalsIgnoreCase("!dehydrate") && channel.equals(chanmatch)))
+      {
+         hydration = false;
+         hours = 0;  
+      }
+      if(message.contains("!add"))
+      { 
+         addcheck = true;
+         if(sender.equals("streamelements") == false && sender.equals("nightbot") == false)
+         {
+            lastadder = sender;
+            chancheck = "#" + lastadder;
+            System.out.println(lastadder);
+         }
+         sendMessage(channel, "!followage " + sender);
+      }
+      if(channel.equals(chancheck) == false && (addcheck == true && (message.contains("is not following") && sender.equals("streamelements"))))
+      {
+         System.out.println("    addcheck is now false");
+         addcheck = false;
+         sendMessage(channel, "/timeout " + lastadder + " 600");
+         sendMessage(channel, "Do you see viewer levels in the title? No. Please don't even bother with add requests if you aren't even followed to the channel at the very least!");
+      }
+      if(addcheck == true && (channel.equals(chancheck) || ((message.contains("is not following") == false) && sender.equals("streamelements"))))
+      {
+         addcheck = false;
+         sendMessage(channel, "!lv");
+         sendMessage(channel, "!lvsheet");
       }
       if(message.equalsIgnoreCase("!interest"))
       {
@@ -157,7 +231,7 @@ public class Bot extends PircBot
          }
          catch (Exception f){};
       }
-      if(message.equalsIgnoreCase("!up"))
+      if(message.equalsIgnoreCase("!up") || message.equalsIgnoreCase("!u"))
       {
          try
          {
@@ -169,7 +243,7 @@ public class Bot extends PircBot
          }
          catch(Exception g){};
       }
-      if(message.equalsIgnoreCase("!down"))
+      if(message.equalsIgnoreCase("!down") || message.equalsIgnoreCase("!d"))
       {
          try
          {
@@ -181,7 +255,7 @@ public class Bot extends PircBot
          }
          catch(Exception g){};  
       }
-      if(message.equalsIgnoreCase("!left"))
+      if(message.equalsIgnoreCase("!left") || message.equalsIgnoreCase("!l"))
       {
          try
          {
@@ -193,7 +267,7 @@ public class Bot extends PircBot
          }
          catch(Exception g){};
       }
-      if(message.equalsIgnoreCase("!right"))
+      if(message.equalsIgnoreCase("!right") || message.equalsIgnoreCase("!r"))
       {
          try
          {
