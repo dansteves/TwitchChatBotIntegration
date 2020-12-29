585bace4e8aff8fe526c5dbe04bf533e94fef25e
Daniel Stevens <35317235+dansteves@users.noreply.github.com>
2020-12-29 14:07:47 -0800

    Update Config.java

--- Config.java
+++ Config.java
@@ -1,12 +1,32 @@
+import java.util.*;
+import java.io.*;
 public class Config
 {
+   public static File input;
+   public static String user = "";
+   public static String auth = "";
    public static String curGame = "";
    public static void main(String [] args) throws Exception
    {
-      //You must run three aruments: your twitch username, the oauth key, and the name of the game you are playing
-      String user = "#" + args[0];
-      String auth = args[1];
-      curGame = args[2];
+      /*
+      
+      Modify the arguments.txt file to a 4 lined .txt file containing:
+      1) Your Twitch username
+      2) Your oauth code generated from "https://twitchapps.com/tmi/"
+      3) The current game you are playing
+      4) Your Twitch bot's username
+      
+      Sample arguments.txt file:
+      
+      TwitchPlaysPokemon
+      oauth:123a4bcdefghijklmn5678o9pqrst0
+      Twitch Plays
+      tpp
+      
+      */
+      
+      handleFile();
+      loadargs();
       Bot bot = new Bot();
       bot.setVerbose(true);
       bot.connect("irc.twitch.tv", 6667, auth);
@@ -14,5 +34,27 @@ public class Config
       bot.sendRawLine("CAP REQ :twitch.tv/commands");
       bot.joinChannel(user);
       bot.getName();
+   }
+   public static boolean handleFile() throws IOException
+   {
+     input = new File("arguments.txt");
+     input.createNewFile();
+     return true;
+   }
+   public static void loadargs() throws FileNotFoundException
+   {
+         Scanner in = new Scanner(input);
+         if(in.hasNextLine())
+         {
+            user = "#" + in.nextLine();
+         }
+         if(in.hasNextLine())
+         {
+            auth = in.nextLine();
+         }
+         if(in.hasNextLine())
+         {
+            curGame = in.nextLine();
+         }
    }
 }
